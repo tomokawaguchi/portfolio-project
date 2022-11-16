79ca3d6ad5a600f5c97934d6f96955c1d68a0af2
Tomoko Kawaguchi <tomo.k.dev@gmail.com>
2022-11-16 19:59:27 +1100

    Added more styling to landing panel

---
+++ script.js
@@ -0,0 +1,29 @@
+// const main = document.getElementById("mainElement");
+// console.log(main);
+// const mainBeforeElement = getComputedStyle(main, "::before");
+// const mainAfterElement = getComputedStyle(main, "::after");
+
+// let panelTitleEng = mainBeforeElement.content;
+// let panelTitleJp = mainAfterElement.content;
+
+// console.log(panelTitleEng);
+// console.log(panelTitleJp);
+
+// const panelTexts = [
+// 	{
+// 		eng: "Hello",
+// 		jp: "こんにちは",
+// 	},
+// 	{
+// 		eng: "About",
+// 		jp: "概要",
+// 	},
+// 	{
+// 		eng: "Projects",
+// 		jp: "プロジェクト",
+// 	},
+// 	{
+// 		eng: "Contact",
+// 		jp: "お問い合わせ",
+// 	},
+// ];
